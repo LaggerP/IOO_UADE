cf45396ac89a5722a418250a29975f69b02016e0
TamaraCabello <56520799+TamaraCabello@users.noreply.github.com>
2019-11-27 14:41:47 -0300

    Update Mostrar_Turnos.java
    Corregido error en la visualización de turnos

--- src/interfaces/Mostrar_Turnos.java
+++ src/interfaces/Mostrar_Turnos.java
@@ -16,27 +16,27 @@ import java.awt.GridLayout;
 public class Mostrar_Turnos extends JFrame {
 
 	public Mostrar_Turnos(Control_Pacientes access, Paciente user) {
-		
+
 //		JLabel lblMainTitle = new JLabel("Usted posee los siguientes turnos asignados a su usuario");
 //		lblMainTitle.setFont(new Font("Tahoma", Font.PLAIN, 17));
 //		lblMainTitle.setHorizontalAlignment(SwingConstants.CENTER);
 //		getContentPane().add(lblMainTitle, BorderLayout.NORTH);
-		
+
 		setTitle("Turnos asignados a su usuario");
-		
+
 		int len = user.mostrar_turnos().size();
-		
+
 		Container c = this.getContentPane();
 		GridLayout lay = new GridLayout(len,2);
 		c.setLayout(lay);
 		
-		JLabel cosas[] = new JLabel[6];
+		JLabel cosas[] = new JLabel[100];
 		for (int i=0;i<len;i++) {
 			cosas[2*i] = new JLabel(user.mostrar_turnos().get(i).getFecha());
 			cosas[(2*i)+1] = new JLabel("     Con el doctor: " + user.mostrar_turnos().get(i).getProfesional());
-			
+
 		}
-		
+
 		for (int i=0;i<len*2;i++) {
 			c.add(cosas[i]);
 		}
